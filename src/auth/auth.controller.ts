902dde7ce8280b971a670ee8c6d15e6fa71eb71f
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2026-02-21 19:57:07 -0300

    feat(auth): add me endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
 import {
-    Controller,
-    Post,
-    UseGuards,
-    Req,
-    Body,
-    Res,
+  Controller,
+  Post,
+  UseGuards,
+  Req,
+  Body,
+  Res,
+  UnauthorizedException,
+  Get,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
@@ -14,37 +16,50 @@ import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService, private configService: ConfigService) { }
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {}
 
-    @UseGuards(AuthGuard('local'))
-    @Post('login')
-    async login(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
-        const { access_token } = await this.authService.login(req.user);
-        res.cookie('access_token', access_token, {
-            httpOnly: true,
-            path: '/',
-            secure: this.configService.getOrThrow<string>('NODE_ENV') === 'production',
-        });
-        return { access_token };
-    }
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+    const { access_token } = await this.authService.login(req.user);
+    res.cookie('access_token', access_token, {
+      httpOnly: true,
+      path: '/',
+      secure:
+        this.configService.getOrThrow<string>('NODE_ENV') === 'production',
+    });
+    return { access_token };
+  }
 
-    @Post('register')
-    async register(
-        @Body() registerDto: RegisterDto,
-        @Res({ passthrough: true }) res: Response,
-    ) {
-        const { access_token } = await this.authService.register(registerDto);
-        res.cookie('access_token', access_token, {
-            httpOnly: true,
-            path: '/',
-            secure: this.configService.getOrThrow<string>('NODE_ENV') === 'production',
-        });
-        return { access_token };
-    }
+  @Post('register')
+  async register(
+    @Body() registerDto: RegisterDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { access_token } = await this.authService.register(registerDto);
+    res.cookie('access_token', access_token, {
+      httpOnly: true,
+      path: '/',
+      secure:
+        this.configService.getOrThrow<string>('NODE_ENV') === 'production',
+    });
+    return { access_token };
+  }
 
-    @Post('logout')
-    async logout(@Res({ passthrough: true }) res: Response) {
-        res.clearCookie('access_token');
-        return { message: 'Logged out successfully' };
-    }
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('access_token');
+    return { message: 'Logged out successfully' };
+  }
+
+  @Get('me')
+  async me(@Req() req: Request) {
+    if (!req.user) throw new UnauthorizedException();
+
+    // @ts-ignore
+    return this.authService.me(req.user.email);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,39 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { RegisterDto } from './dto/register.dto';
+import { NotFound } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private usersService: UsersService,
-        private jwtService: JwtService,
-    ) { }
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-    async validateUser(email: string, pass: string): Promise<any> {
-        const user = await this.usersService.findByEmail(email);
-        if (user && (await bcrypt.compare(pass, user.password))) {
-            const { password, ...result } = user;
-            return result;
-        }
-        return null;
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && (await bcrypt.compare(pass, user.password))) {
+      const { password, ...result } = user;
+      return result;
     }
+    return null;
+  }
 
-    async login(user: any) {
-        const payload = { email: user.email, sub: user.id };
-        return {
-            access_token: this.jwtService.sign(payload),
-        };
-    }
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 
-    async register(registerDto: RegisterDto) {
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(registerDto.password, salt);
-        const user = await this.usersService.create({
-            ...registerDto,
-            password: hashedPassword,
-        });
-        return this.login(user);
-    }
+  async register(registerDto: RegisterDto) {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(registerDto.password, salt);
+    const user = await this.usersService.create({
+      ...registerDto,
+      password: hashedPassword,
+    });
+    return this.login(user);
+  }
+
+  async me(email: string) {
+    const user = await this.usersService.findByEmail(email);
+
+    if (!user) throw new NotFoundException('User not found');
+
+    const {
+      password,
+      createdAt,
+      isActive,
+      updatedAt,
+      profilePic,
+      phone,
+      ...result
+    } = user;
+    return result;
+  }
 }
